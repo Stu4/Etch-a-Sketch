5384fc1b64aa05db3ceb02a56933c56b8ed047d7
Stuart92 <stuart.malone@hotmail.com>
2020-10-15 10:26:19 +0100

    First attempt complete, some formatting still to be done

--- etchasketch.js
+++ etchasketch.js
@@ -1,13 +1,54 @@
-const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+
+const container = document.querySelector("#container");
+const resetButton = document.querySelector("#reset");
+
+let slider = document.getElementById("slider");
+let sliderValue = document.getElementById("sliderValue");
+
+let gridSize = slider.value;
+var gridBoxes;
+
+gridCreate();
+
+// Slider Event Listeners
+
+slider.addEventListener("input", displaySliderValue);
+slider.addEventListener("input", gridCreate);
+
+function displaySliderValue() {
+    sliderValue.textContent = this.value;
+}
+
+function gridCreate(gridSize) {
+    container.innerHTML = "";
+    gridSize = slider.value;
+    var toAdd = document.createDocumentFragment();
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
-        container.appendChild(cell).className = "grid-item";
+        cell.classList.add("gridBox");
+        container.appendChild(cell)
         };
+        gridBoxes = document.querySelectorAll(".gridBox");
+        gridBoxes.forEach((gridBox) => {
+            gridBox.addEventListener("mouseover", (e) => {
+                gridBox.classList.add("active");
+            });
+        })
+
     };
 
-    makeRows(16, 16);
+    //Add Clear Button to remove 'active' cells
+
+    clear.addEventListener("click", (e) => {
+        gridBoxes.forEach((gridBox) => {
+            gridBox.classList.remove("active");
+        });
+    })
+
+    gridCreate(gridSize);
+    
